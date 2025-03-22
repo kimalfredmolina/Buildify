980a37c6b9d8b84d8ad2aa1c6298e46b4efa1b67
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-03-22 21:59:27 +0800

    first commit

---
+++ landingpage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyDraft Landing Page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="flex items-center justify-center min-h-screen bg-white">
+    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    <div class="text-center">
+        <h1 class="text-7xl mr-40 font-medium text-white">Welcome to</h1>
+        <h2 class="text-9xl font-bold text-white">EasyDraft</h2>
+        <p class="mt-6 text-3xl text-white">Create Account now! for free</p>
+        <button class="group relative mt-10">
+            <div class="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-16 font-medium text-white transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3">Sign Up Now!</div>
+            <div class="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5]"></div>
+        </button>
+    </div>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./*.php",
+      "./path/to/your/templates/**/*.php",
+      "./path/to/your/components/**/*.php"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
