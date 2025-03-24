3f38d9e1ca6416eb8fc0bfa80ce4f2781255a522
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-03-24 18:50:41 +0800

    Update landing page to add signup button functionality

--- landingpage.php
+++ landingpage.php
@@ -14,8 +14,12 @@
         <h1 class="text-7xl mr-40 font-medium text-white">Welcome to</h1>
         <h2 class="text-9xl font-bold text-white">BuildiFy</h2>
         <p class="mt-6 text-3xl text-white">Create Account now! for free</p>
-        <button class="group relative mt-10">
-            <div class="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-16 font-medium text-white transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3">Sign Up Now!</div>
+        <button
+            onclick="window.location.href='/php/Signup.php'"
+            class="group relative mt-10">
+            <div class="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-16 font-medium text-white transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3">
+                Sign Up Now!
+            </div>
             <div class="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5]"></div>
         </button>
     </div>
